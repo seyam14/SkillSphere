1624b4fc4477fbf5e90aace64850fa60fe33aafe
seyam <mdseyam3215@gmail.com>
2024-01-22 17:10:37 +0600

    first commit

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,41 @@
+
+
+const Banner = () => {
+    return (
+        <div className="mt-4">
+        <div
+          className="hero h-[75vh]"
+          style={{ backgroundImage: "url(https://i.ibb.co/tsdk9zL/e-learning-management-system.jpg)" }}
+        >
+          <div className="hero-overlay bg-opacity-75"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="w-[600px">
+              <h2 className="mb-5 text-2xl font-bold ">
+              Learnify: Ignite, Illuminate, and Inspire
+              </h2>
+              <p>
+              Embark on a journey of intellectual excellence with WisdomWave, where learning is not just a process but
+                <br />
+                <span className="font-semibold">
+                a dynamic exploration of limitless possibilities.
+                </span>
+              </p>
+              <div className="inline-flex mt-4">
+                <div className="text-center">
+                  <input
+                    type="text"
+                    placeholder="Search here...."
+                    className=" rounded-l py-2 px-4 outline-none"
+                  />
+                  <button className="bg-red-500 text-white rounded-r py-2 px-4 hover:bg-blue-600">
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+export default Banner;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
+import Home from './Pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
