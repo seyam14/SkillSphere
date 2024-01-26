60f4281084283a899a04db7277922b2ef3e3598a
seyam <mdseyam3215@gmail.com>
2024-01-26 19:25:21 +0600

    category

--- src/Pages/Category/CategoryCard.jsx
+++ src/Pages/Category/CategoryCard.jsx
@@ -6,7 +6,7 @@ const CategoryCard = ({cart}) => {
  
     const  {
         _id,
-        courseTitle,
+        CourseTitle,
         DeadLine,
         Description,
         Price,
@@ -15,12 +15,12 @@ const CategoryCard = ({cart}) => {
       return (
         <div className="card lg:card-side bg-sky-300 md:h-[300px] my-8 shadow-xl">
          <div className="card-body">
-          <h2 className="card-title "><span className="font-bold ">Course Title: </span> {courseTitle}</h2>
-          <h2 ><span className="font-bold "> DeadLine:</span> {DeadLine}</h2>
+          <h2 className="card-title "><span className="font-bold ">Course Title: </span> {CourseTitle}</h2>
+          <h2 ><span className="font-bold "> Course DeadLine:</span> {DeadLine}</h2>
           <h2><span className="font-bold "> Price : </span>{Price}</h2>
           <h2><span className="font-bold ">Description: </span>{Description}</h2>
           <Link to ={`/courseDetails/${_id}`} >
-            <button className="btn btn-neutral">Bid Now</button>
+            <button className="btn btn-neutral">More details</button>
           </Link>
         </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        loader : () => fetch('http://localhost:5000/addcourse'),
       },
       {
         path: '/login',
