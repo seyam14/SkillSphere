bfcade002d4b1717934e31153aecdb559916eba9
seyam <mdseyam3215@gmail.com>
2024-04-20 00:49:29 +0600

    dashboard user updated

--- src/Dashboard components/UserHome/UserHome.jsx
+++ src/Dashboard components/UserHome/UserHome.jsx
@@ -5,20 +5,20 @@ const UserHome = () => {
     const { user } = useAuth();
     return (
         <div>
-               <h2 className="text-3xl">
+            <img
+            src={img}
+            alt=''
+            className="max-w-full h-auto border shadow-xl "
+            style={{ maxHeight: '450px', width: '100%' }}
+            />
+            <h2 className="text-3xl">
                 <span>Dear,User</span>
                 {
                     user?.displayName ? user.displayName : 'Back'
                 }
             </h2>
             <br />
-             <p className="text-xl text-gray-500">Check your  progress </p> 
-            <img
-            src={img}
-            alt=''
-            className="max-w-full h-auto border shadow-xl "
-            style={{ maxHeight: '450px', width: '100%' }}
-          />
+            <p className="text-xl text-gray-500">Check your  progress </p> 
         </div>
     );
 };
