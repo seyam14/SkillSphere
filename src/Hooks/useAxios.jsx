4c6f832219a109120a6b7f3ef51915be3f60adc6
seyam <mdseyam3215@gmail.com>
2024-04-20 00:08:48 +0600

    hook updated

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://skillsphere-server-side.vercel.app'
 })
 
 const useAxios = () => {
